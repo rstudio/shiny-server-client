fcbd2398c9ee87422d66c1c0b5af8091ef280e23
Aron Atkins <aron@gweep.net>
2021-03-23 09:53:11 -0400

    Support for SockJS 1.0+; remove old SockJS support.
    The SockJS option protocol_whitelist was only valid with pre-1.0 releases.
    Newer releases use the transports option.
    
    The protocol chooser is updated to support "eventsource" and "htmlfile".
    
    The option transportDebugging is no longer supported.

--- test/sockjs.js
+++ test/sockjs.js
@@ -13,10 +13,10 @@ describe("sockjs factory", () => {
   before(() => {
     sockjsOriginal = global.SockJS;
     // mimic signature from
-    // https://github.com/sockjs/sockjs-client/blob/v0.3.4/lib/sockjs.js#L9
+    // https://github.com/sockjs/sockjs-client/blob/v1.5.0/lib/main.js#L32
     // and just return an object with those values.
-    global.SockJS = (url, dep_protocols_whitelist, options) => {
-      return {url, dep_protocols_whitelist, options};
+    global.SockJS = (url, deprecated_protocols, options) => {
+      return {url, deprecated_protocols, options};
     };
     protocolChooser = {whitelist: ["alpha", "bravo", "charley"]};
   });
@@ -25,72 +25,42 @@ describe("sockjs factory", () => {
     global.SockJS = sockjsOriginal;
   });
   
-  it("does not configure debugging when transport debugging is not specified", (done) => {
+  it("with no disabled protocols, all protocols are enumerated as transports", (done) => {
     let factory = sockjs.createFactory(
       protocolChooser,
       {});
     factory(targetURL, context, (_, conn) => {
-      assert.equal(conn.options.debug, false);
+      assert.deepEqual(conn.options.transports, ["alpha", "bravo", "charley"]);
       done();
     });
   });
 
-  it("does not configure debugging when transport debugging is disabled", (done) => {
-    let factory = sockjs.createFactory(
-      protocolChooser,
-      {transportDebugging: false});
-    factory(targetURL, context, (_, conn) => {
-      assert.equal(conn.options.debug, false);
-      done();
-    });
-  });
-  
-  it("does configures debugging when transport debugging is enabled", (done) => {
-    let factory = sockjs.createFactory(
-      protocolChooser,
-      {transportDebugging: true});
-    factory(targetURL, context, (_, conn) => {
-      assert.equal(conn.options.debug, true);
-      done();
-    });
-  });
-  
-  it("with no disabled protocols, all protocols are enumerated", (done) => {
-    let factory = sockjs.createFactory(
-      protocolChooser,
-      {});
-    factory(targetURL, context, (_, conn) => {
-      assert.deepEqual(conn.options.protocols_whitelist, ["alpha", "bravo", "charley"]);
-      done();
-    });
-  });
-
-  it("with an empty set of disabled protocols, all protocols are enumerated", (done) => {
+  it("with an empty set of disabled protocols, all protocols are enumerated as transports", (done) => {
     let factory = sockjs.createFactory(
       protocolChooser,
       {disableProtocols: []});
     factory(targetURL, context, (_, conn) => {
-      assert.deepEqual(conn.options.protocols_whitelist, ["alpha", "bravo", "charley"]);
+      assert.deepEqual(conn.options.transports, ["alpha", "bravo", "charley"]);
       done();
     });
   });
 
-  it("with a subset of protocols disabled, only the remaining protocols are included", (done) => {
+  it("with a subset of protocols disabled, only the remaining protocols are included as transports", (done) => {
     let factory = sockjs.createFactory(
       protocolChooser,
       {disableProtocols: ["bravo"]});
     factory(targetURL, context, (_, conn) => {
-      assert.deepEqual(conn.options.protocols_whitelist, ["alpha", "charley"]);
+      assert.deepEqual(conn.options.transports, ["alpha", "charley"]);
       done();
     });
   });
 
-  it("with all protocols disabled, a dummy protocol is included", (done) => {
+  it("with all protocols disabled, a dummy protocol is included as transport", (done) => {
     let factory = sockjs.createFactory(
       protocolChooser,
       {disableProtocols: ["alpha", "bravo", "charley"]});
     factory(targetURL, context, (_, conn) => {
-      assert.deepEqual(conn.options.protocols_whitelist, ["dummy"]);
+      assert.deepEqual(conn.options.transports, ["dummy"]);
       done();
     });
   });
